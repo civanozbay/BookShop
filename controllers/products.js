ace2e9f33e19b94321a5cbd73e708995fcddf531
civanozbay <civan417@hotmail.com>
2023-04-07 17:03:49 +0200

    engines are added for new pages

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 
 exports.getAddProduct = (req,res,next) => {
-    res.render('add-product',{
+    res.render('admin/add-product',{
         pageTitle:'Add Product',
         path:'/admin/add-product'
     })
@@ -22,7 +22,7 @@ exports.getProducts = (req,res,next) => {
     // since we implement pug as a template engine we don't need to send html no more.
     // res.sendFile(path.join(__dirname,'../','views','shop.html'));
     Product.fetchAll(products => {
-        res.render('shop',
+        res.render('shop/product-list',
         {prods:products,
         pageTitle : 'Shop',
         path:'/shop'}); 
